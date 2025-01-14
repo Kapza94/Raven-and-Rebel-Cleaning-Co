6d5523eda94275ce51308fc4490ff786680441c4
Kapza <lukakapetanovic@icloud.com>
2025-01-14 15:39:03 +0100

    fixed carousel size V3.

--- src/pages/services.tsx
+++ src/pages/services.tsx
@@ -6,136 +6,118 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import "../styles/globals.css";
-import "../styles/about.css";
+import "../styles/services.css";
+import Image from "next/image";
+import { useEffect } from "react";
 
 const Services = () => {
+  useEffect(() => {
+    // Set the swiper navigation size dynamically
+    document.documentElement.style.setProperty("--swiper-navigation-size", "20px");
+  }, []);
   return (
     <>
-      <Swiper
-        modules={[Navigation, Pagination, Scrollbar, A11y]}
-        spaceBetween={50}
-        slidesPerView={3}
-        navigation
-        pagination={{ clickable: true }}
-        scrollbar={{ draggable: true }}
-        onSwiper={(swiper) => console.log(swiper)}
-        onSlideChange={() => console.log("slide change")}
-      >
-        <div className="container mx-auto px-4 py-8">
-          <h1 className="text-4xl font-bold text-center mb-8">Our Services</h1>
-          <div className="slider-container"></div>
+      <div className="container mx-auto px-4 py-8 flex flex-col items-start">
+        <h1 className="text-4xl font-bold text-center mb-8">Our Services</h1>{" "}
+        <Swiper
+          modules={[Navigation, Pagination, Scrollbar, A11y]}
+          spaceBetween={50}
+          slidesPerView={3}
+          navigation
+          pagination={{ clickable: true }}
+          // scrollbar={{ draggable: true }}
+          loop={true}
+          onSwiper={(swiper) => console.log(swiper)}
+          onSlideChange={() => console.log("slide change")}
+        >
           <SwiperSlide>
             <div className="card">
-              <h2 className="text-2xl">Home cleaning</h2>
-              <p>
-                Transform your home with our Full Home Cleaning service. We thoroughly clean and
-                sanitize bedrooms, living areas, kitchens, and bathrooms, leaving every corner
-                spotless. Perfect for regular upkeep or pre- and post-move cleaning, we make your
-                space fresh, clean, and inviting.
+              <Image
+                className="card-style"
+                src="/cards/home.jpg"
+                width={400}
+                height={200}
+                alt="Picture of the author"
+              />
+              <h2 className="tracking-wide text-2xl m-5 font-bold">Home cleaning</h2>
+              <p className="pr-4 pl-8 text-base font-normal text-white/80 leading-relaxed">
+                Transform your home with our Full Home Cleaning service. We clean and sanitize
+                bedrooms, living areas, kitchens, and bathrooms, leaving every corner spotless.
+                Perfect for regular upkeep, we make your space fresh and inviting.
               </p>
             </div>
           </SwiperSlide>
           <SwiperSlide>
             <div className="card">
-              <h2 className="text-2xl">Office cleaning</h2>
-              <p>
-                Keep your workspace spotless and productive with our Office Cleaning service. We
-                handle everything from desks and floors to common areas and restrooms, ensuring a
-                clean and professional environment. Perfect for regular maintenance or one-time deep
-                cleans, we help create a healthier, more inviting office for your team and clients
+              <Image
+                className="card-style"
+                src="/cards/office.jpg"
+                width={400}
+                height={200}
+                alt="Picture of the author"
+              />
+              <h2 className="tracking-wide text-2xl m-5 font-bold">Office cleaning</h2>
+              <p className="pr-4 pl-8 text-base font-normal text-white/80 leading-relaxed">
+                Keep your workspace spotless with our Office Cleaning service. We clean desks,
+                floors, common areas, and restrooms, ensuring a professional environment. Perfect
+                for regular maintenance or deep cleans, we create a healthier, inviting office.
               </p>
             </div>
           </SwiperSlide>
           <SwiperSlide>
             <div className="card">
-              <h2 className="text-2xl">Office cleaning</h2>
-              <p>
-                Keep your workspace spotless and productive with our Office Cleaning service. We
-                handle everything from desks and floors to common areas and restrooms, ensuring a
-                clean and professional environment. Perfect for regular maintenance or one-time deep
-                cleans, we help create a healthier, more inviting office for your team and clients
+              <Image
+                className="card-style"
+                src="/cards/carpetcleaning.jpg"
+                width={400}
+                height={200}
+                alt="Picture of the author"
+              />
+              <h2 className="tracking-wide text-2xl m-5 font-bold">Carpet cleaning</h2>
+              <p className="pr-4 pl-8 text-base font-normal text-white/70 leading-relaxed">
+                Refresh your carpets with our professional Carpet Cleaning service, removing dirt,
+                stains, and allergens. Perfect for regular maintenance or deep cleaning to extend
+                your carpet&apos;s life and keep your home fresh.
               </p>
             </div>
           </SwiperSlide>
           <SwiperSlide>
             <div className="card">
-              <h2 className="text-2xl">Post construction cleaning</h2>
-              <p>
-                Make your newly built or renovated space move-in ready with our professional
-                Post-Construction Cleaning service. We expertly remove dust, debris, and leftover
-                construction residue from all surfaces, ensuring a spotless and polished finish.
-                Ideal for homes, offices, and renovation projects, our detailed cleaning transforms
-                your space into a clean, safe, and welcoming environment.
+              <Image
+                className="card-style"
+                src="/cards/postconstruction.jpg"
+                width={400}
+                height={200}
+                alt="Picture of the author"
+              />
+              <h2 className="tracking-wide text-2xl m-5 font-bold">Post construction cleaning</h2>
+              <p className="pr-4 pl-8 text-base font-normal text-white/80 leading-relaxed">
+                Our Post-Construction Cleaning service removes dust, debris, and residue from newly
+                built or renovated spaces. Ideal for making your home or office spotless and move-in
+                ready.
               </p>
             </div>
           </SwiperSlide>
           <SwiperSlide>
             <div className="card">
-              <h2 className="text-2xl">Post Occupation cleaning</h2>
-              <p>
-                Leave your old space spotless and ready for the next occupants with our professional
-                Post-Occupation Cleaning service. We thoroughly clean and sanitize every room,
-                removing dust, grime, and residues to ensure the property looks its best. Perfect
-                for homes and offices, our detailed cleaning provides a fresh start for new
-                occupants while saving you time and effort.
+              <Image
+                className="card-style"
+                src="/cards/postoccupationcleaning.jpg"
+                width={400}
+                height={200}
+                alt="Picture of the author"
+              />
+              <h2 className="tracking-wide text-2xl m-5 font-bold">Post Occupation cleaning</h2>
+              <p className="pr-4 pl-8 text-base font-normal text-white/80 leading-relaxed">
+                Leave your old space spotless with our Post-Occupation Cleaning service, thoroughly
+                cleaning and sanitizing every area. Perfect for providing a fresh, clean start for
+                new occupants in homes or offices.
               </p>
             </div>
           </SwiperSlide>
           ...
-        </div>
-      </Swiper>
-      {/* <div className="container mx-auto px-4 py-8">
-        <h1 className="text-4xl font-bold text-center mb-8">Our Services</h1>
-        <div className="slider-container"> */}
-      {/* <div className="card">
-            <h2 className="text-2xl">Home cleaning</h2>
-            <p>
-              Transform your home with our Full Home Cleaning service. We thoroughly clean and
-              sanitize bedrooms, living areas, kitchens, and bathrooms, leaving every corner
-              spotless. Perfect for regular upkeep or pre- and post-move cleaning, we make your
-              space fresh, clean, and inviting.
-            </p>
-          </div> */}
-      {/* <div className="card">
-            <h2 className="text-2xl">Office cleaning</h2>
-            <p>
-              Keep your workspace spotless and productive with our Office Cleaning service. We
-              handle everything from desks and floors to common areas and restrooms, ensuring a
-              clean and professional environment. Perfect for regular maintenance or one-time deep
-              cleans, we help create a healthier, more inviting office for your team and clients
-            </p>
-          </div> */}
-      {/* <div className="card">
-            <h2 className="text-2xl">Carpet cleaning</h2>
-            <p>
-              Refresh your carpets with our Carpet Cleaning service. We use professional-grade
-              equipment and eco-friendly solutions to remove dirt, stains, and allergens, leaving
-              your carpets clean and fresh. Perfect for regular maintenance or one-time deep cleans,
-              we help extend the life of your carpets and create a healthier home environment.
-            </p>
-          </div> */}
-      {/* <div className="card">
-            <h2 className="text-2xl">Post construction cleaning</h2>
-            <p>
-              Make your newly built or renovated space move-in ready with our professional
-              Post-Construction Cleaning service. We expertly remove dust, debris, and leftover
-              construction residue from all surfaces, ensuring a spotless and polished finish. Ideal
-              for homes, offices, and renovation projects, our detailed cleaning transforms your
-              space into a clean, safe, and welcoming environment.
-            </p>
-          </div> */}
-      {/* <div className="card">
-            <h2 className="text-2xl">Post Occupation cleaning</h2>
-            <p>
-              Leave your old space spotless and ready for the next occupants with our professional
-              Post-Occupation Cleaning service. We thoroughly clean and sanitize every room,
-              removing dust, grime, and residues to ensure the property looks its best. Perfect for
-              homes and offices, our detailed cleaning provides a fresh start for new occupants
-              while saving you time and effort.
-            </p>
-          </div> */}
-      {/* </div>
-      </div> */}
+        </Swiper>
+      </div>
     </>
   );
 };
