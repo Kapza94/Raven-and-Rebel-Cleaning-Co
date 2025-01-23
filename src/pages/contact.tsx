3f414783297c906ea4fe67f69e20e05e4bbf8399
Kapza <lukakapetanovic@icloud.com>
2025-01-23 21:53:12 +0100

    success message activated and working.

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -7,6 +7,7 @@ const Contact = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
+  const [isSent, setIsSent] = useState(false);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -33,6 +34,7 @@ const Contact = () => {
       .send(serviceId, templateId, templateParams, publicKey)
       .then((response) => {
         console.log("Email was sent successfully", response);
+        setIsSent(true);
         setName("");
         setEmail("");
         setMessage("");
@@ -43,71 +45,84 @@ const Contact = () => {
   };
 
   return (
-    <div className="mx-auto px-4 py-8 flex flex-col items-center bg-white rounded-lg">
-      <h2 className="text-3xl font-bold mb-6">Contact Us</h2>
-      <form onSubmit={handleSubmit} className="w-full max-w-lg">
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
-            Name
-          </label>
-          <div className="relative">
-            <span>
-              <FaUser className="input-icon" />
-            </span>
-            <input
-              type="text"
-              placeholder="Full name "
-              id="name"
-              name="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              className="input-field bg-gray-100 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              required
-            />
-          </div>
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-            Email
-          </label>
-          <div className="relative">
-            <FaRegEnvelope className="input-icon" />
-            <input
-              type="email"
-              id="email"
-              placeholder="Your email here..."
-              name="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className=" input-field bg-gray-100 shadow appearance-none border rounded w-full py-2  text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              required
-            />
-          </div>
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="message">
-            Message
-          </label>
-          <textarea
-            id="message"
-            placeholder="Your message here..."
-            name="message"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            className="input-field-msg bg-gray-100 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            required
-          />
-        </div>
-        <div className="flex items-center justify-between">
-          <button
-            id="submit-button"
-            type="submit"
-            className=" bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+    <div className="wrapper">
+      <div className="thank-you-msg">
+        {isSent && (
+          <div
+            className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative"
+            role="alert"
           >
-            Send Message
-          </button>
-        </div>
-      </form>
+            <strong className="font-bold">Thank you for contacting us!</strong>
+            <span className="block sm:inline">We will get back to you as soon as possible.</span>
+          </div>
+        )}
+      </div>
+      <div className="form mx-auto px-4 py-8 flex flex-col items-center bg-white rounded-lg">
+        <h2 className="text-3xl font-bold mb-6">Contact Us</h2>
+        <form onSubmit={handleSubmit} className="w-full max-w-lg">
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
+              Name
+            </label>
+            <div className="relative">
+              <span>
+                <FaUser className="input-icon" />
+              </span>
+              <input
+                type="text"
+                placeholder="Full name "
+                id="name"
+                name="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className="input-field bg-gray-100 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                required
+              />
+            </div>
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+              Email
+            </label>
+            <div className="relative">
+              <FaRegEnvelope className="input-icon" />
+              <input
+                type="email"
+                id="email"
+                placeholder="Your email here..."
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className=" input-field bg-gray-100 shadow appearance-none border rounded w-full py-2  text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                required
+              />
+            </div>
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="message">
+              Message
+            </label>
+            <textarea
+              id="message"
+              placeholder="Your message here..."
+              name="message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              className="input-field-msg bg-gray-100 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              required
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              id="submit-button"
+              type="submit"
+              className=" bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+              Send Message
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
