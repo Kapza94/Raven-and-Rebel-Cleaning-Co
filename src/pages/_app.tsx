4dad9e8814c495ce089a85f347229864ed7f4b27
Kapza <lukakapetanovic@icloud.com>
2025-01-26 00:25:17 +0100

    added meta tag for validation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,12 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 //
 
 export default function App({ Component, pageProps }: AppProps) {
+  <Head>
+    <meta name="google-site-verification" content="g-abV7Ux6p4YPW4z_83unxZr2SXsE0_kEWgLIdwCEuQ" />
+  </Head>;
   return <Component {...pageProps} />;
 }
